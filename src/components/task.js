1c5629300117ad81c5ae4747cc8adf9ceba1c29e
Grabelnikov Rus <grabelnikov.rus@mail.ru>
2020-11-21 20:01:03 +0700

    12345

--- src/components/task.js
+++ src/components/task.js
@@ -8,6 +8,9 @@ const Task = (props) => {
   const { name, completed, editing, id, date } = task;
 
   const [dateCreate, setDateCreate] = useState(formatDistanceToNow(date, { includeSeconds: true }));
+  const [timePlay, setTimePlay] = useState(null);
+  const [time, setTime] = useState('00:00');
+  const [isPlay, setIsPlay] = useState(false)
 
   useEffect(() => {
       let timer = setInterval(() => setDateCreate(formatDistanceToNow(date, { includeSeconds: true })), 2000);
@@ -22,6 +25,37 @@ const Task = (props) => {
     if (event.key === "Escape") {
       onEditNameTask(id, name)
     }
+  }
+
+  const onPlay = () => {
+    if (isPlay) return;
+    const value = localStorage.getItem(name);
+    if (value) {
+      let [min, sec] = value.split(':');
+      localStorage.removeItem(name);
+      setTimePlay(new Date() - (Number(min) * 60 + Number(sec)) * 1000)
+    } else {
+      setTimePlay(new Date());
+    }
+    setIsPlay(true);
+  }
+
+  const onPause = () => {
+    if (!isPlay) return;
+    setIsPlay(false);
+    localStorage.setItem(name, `${time}`);
+  }
+
+  const getConversionTime = () => {
+    const diff = Math.floor((new Date() - timePlay)/1000);
+    let min = Math.floor(diff / 60);
+    let sec = diff - min * 60;
+
+    if (min < 10) min = '0' + min;
+    if (sec < 10) sec = '0' + sec;
+    if (sec === 60) sec = '00';
+
+    setTime(`${min}:${sec}`);
   }
 
   if (editing) {
@@ -40,7 +74,8 @@ const Task = (props) => {
         <input className="toggle" type="checkbox" onChange={() => onCompletedTask(id)} checked={completed} />
         <label>
           <span className="title">{name}</span>
-          {!completed && <Clock name={name} />}
+          {!completed && <Clock name={name} time={time} timePlay={timePlay} onPlay={onPlay} onPause={onPause} 
+            isPlay={isPlay} getConversionTime={getConversionTime} />}
           <span className="created">{dateCreate}</span>
         </label>
         {!completed && (
